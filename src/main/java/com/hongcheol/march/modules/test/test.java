91cbc92ea83cc70a86e84bbd82d9d56b2ee23daf
hongcheol94 <macotong3@naver.com>
2022-11-22 17:38:54 +0900

    채팅

---
+++ src/main/java/com/hongcheol/march/modules/test/test.java
@@ -0,0 +1,265 @@
+package com.hongcheol.march.modules.test;
+
+import java.util.Date;
+
+public class test {
+
+	private Integer ifmmSeq;
+	private Integer ifmmAdminNy;
+	private Integer ifmmDomancyNy;
+	private String ifmmname;
+	private String fimmNameEng;
+	private String ifmmLastName;
+	private String ifmmFirstName;
+	private String ifmmLastNameEng;
+	private String ifmmFirstNameEng;
+	private String iffId;
+	private String ifmmPassword;
+	private Date PwdModDate;
+	private String Nickname;
+	private Integer ifmmGenderCd;
+	private Date ifmmDob;
+	private Integer ifmmSavedCd;
+	private Integer ifmmMarriageCd;
+	private Date ifmmMarriageDate;
+	private Integer ifmmChildrenNum;
+	private String ifmmFavoriteColor;
+	private Integer ifmmRecommenderSeq;
+	private Integer ifmmEamilConsentNy;
+	private Integer ifmmSmsConsentNy;
+	private Integer ifmmPushConsentNy;
+	private String ifmmDesc;
+	private Integer ifmmDelNy;
+	private String regIp;
+	private Integer regSeq;
+	private Integer regDeviceCd;
+	private Date regDateTime;
+	private Date regDateTimeSvr;
+	private String modIp;
+	private Integer modSeq;
+	private Integer modDeviceCd;
+	private Date modDateTime;
+	private Date modDateTimeSvr;
+	
+//	----------------------------------------------
+	
+	public Integer getIfmmSeq() {
+		return ifmmSeq;
+	}
+	public void setIfmmSeq(Integer ifmmSeq) {
+		this.ifmmSeq = ifmmSeq;
+	}
+	public Integer getIfmmAdminNy() {
+		return ifmmAdminNy;
+	}
+	public void setIfmmAdminNy(Integer ifmmAdminNy) {
+		this.ifmmAdminNy = ifmmAdminNy;
+	}
+	public Integer getIfmmDomancyNy() {
+		return ifmmDomancyNy;
+	}
+	public void setIfmmDomancyNy(Integer ifmmDomancyNy) {
+		this.ifmmDomancyNy = ifmmDomancyNy;
+	}
+	public String getIfmmname() {
+		return ifmmname;
+	}
+	public void setIfmmname(String ifmmname) {
+		this.ifmmname = ifmmname;
+	}
+	public String getFimmNameEng() {
+		return fimmNameEng;
+	}
+	public void setFimmNameEng(String fimmNameEng) {
+		this.fimmNameEng = fimmNameEng;
+	}
+	public String getIfmmLastName() {
+		return ifmmLastName;
+	}
+	public void setIfmmLastName(String ifmmLastName) {
+		this.ifmmLastName = ifmmLastName;
+	}
+	public String getIfmmFirstName() {
+		return ifmmFirstName;
+	}
+	public void setIfmmFirstName(String ifmmFirstName) {
+		this.ifmmFirstName = ifmmFirstName;
+	}
+	public String getIfmmLastNameEng() {
+		return ifmmLastNameEng;
+	}
+	public void setIfmmLastNameEng(String ifmmLastNameEng) {
+		this.ifmmLastNameEng = ifmmLastNameEng;
+	}
+	public String getIfmmFirstNameEng() {
+		return ifmmFirstNameEng;
+	}
+	public void setIfmmFirstNameEng(String ifmmFirstNameEng) {
+		this.ifmmFirstNameEng = ifmmFirstNameEng;
+	}
+	public String getIffId() {
+		return iffId;
+	}
+	public void setIffId(String iffId) {
+		this.iffId = iffId;
+	}
+	public String getIfmmPassword() {
+		return ifmmPassword;
+	}
+	public void setIfmmPassword(String ifmmPassword) {
+		this.ifmmPassword = ifmmPassword;
+	}
+	public Date getPwdModDate() {
+		return PwdModDate;
+	}
+	public void setPwdModDate(Date pwdModDate) {
+		PwdModDate = pwdModDate;
+	}
+	public String getNickname() {
+		return Nickname;
+	}
+	public void setNickname(String nickname) {
+		Nickname = nickname;
+	}
+	public Integer getIfmmGenderCd() {
+		return ifmmGenderCd;
+	}
+	public void setIfmmGenderCd(Integer ifmmGenderCd) {
+		this.ifmmGenderCd = ifmmGenderCd;
+	}
+	public Date getIfmmDob() {
+		return ifmmDob;
+	}
+	public void setIfmmDob(Date ifmmDob) {
+		this.ifmmDob = ifmmDob;
+	}
+	public Integer getIfmmSavedCd() {
+		return ifmmSavedCd;
+	}
+	public void setIfmmSavedCd(Integer ifmmSavedCd) {
+		this.ifmmSavedCd = ifmmSavedCd;
+	}
+	public Integer getIfmmMarriageCd() {
+		return ifmmMarriageCd;
+	}
+	public void setIfmmMarriageCd(Integer ifmmMarriageCd) {
+		this.ifmmMarriageCd = ifmmMarriageCd;
+	}
+	public Date getIfmmMarriageDate() {
+		return ifmmMarriageDate;
+	}
+	public void setIfmmMarriageDate(Date ifmmMarriageDate) {
+		this.ifmmMarriageDate = ifmmMarriageDate;
+	}
+	public Integer getIfmmChildrenNum() {
+		return ifmmChildrenNum;
+	}
+	public void setIfmmChildrenNum(Integer ifmmChildrenNum) {
+		this.ifmmChildrenNum = ifmmChildrenNum;
+	}
+	public String getIfmmFavoriteColor() {
+		return ifmmFavoriteColor;
+	}
+	public void setIfmmFavoriteColor(String ifmmFavoriteColor) {
+		this.ifmmFavoriteColor = ifmmFavoriteColor;
+	}
+	public Integer getIfmmRecommenderSeq() {
+		return ifmmRecommenderSeq;
+	}
+	public void setIfmmRecommenderSeq(Integer ifmmRecommenderSeq) {
+		this.ifmmRecommenderSeq = ifmmRecommenderSeq;
+	}
+	public Integer getIfmmEamilConsentNy() {
+		return ifmmEamilConsentNy;
+	}
+	public void setIfmmEamilConsentNy(Integer ifmmEamilConsentNy) {
+		this.ifmmEamilConsentNy = ifmmEamilConsentNy;
+	}
+	public Integer getIfmmSmsConsentNy() {
+		return ifmmSmsConsentNy;
+	}
+	public void setIfmmSmsConsentNy(Integer ifmmSmsConsentNy) {
+		this.ifmmSmsConsentNy = ifmmSmsConsentNy;
+	}
+	public Integer getIfmmPushConsentNy() {
+		return ifmmPushConsentNy;
+	}
+	public void setIfmmPushConsentNy(Integer ifmmPushConsentNy) {
+		this.ifmmPushConsentNy = ifmmPushConsentNy;
+	}
+	public String getIfmmDesc() {
+		return ifmmDesc;
+	}
+	public void setIfmmDesc(String ifmmDesc) {
+		this.ifmmDesc = ifmmDesc;
+	}
+	public Integer getIfmmDelNy() {
+		return ifmmDelNy;
+	}
+	public void setIfmmDelNy(Integer ifmmDelNy) {
+		this.ifmmDelNy = ifmmDelNy;
+	}
+	public String getRegIp() {
+		return regIp;
+	}
+	public void setRegIp(String regIp) {
+		this.regIp = regIp;
+	}
+	public Integer getRegSeq() {
+		return regSeq;
+	}
+	public void setRegSeq(Integer regSeq) {
+		this.regSeq = regSeq;
+	}
+	public Integer getRegDeviceCd() {
+		return regDeviceCd;
+	}
+	public void setRegDeviceCd(Integer regDeviceCd) {
+		this.regDeviceCd = regDeviceCd;
+	}
+	public Date getRegDateTime() {
+		return regDateTime;
+	}
+	public void setRegDateTime(Date regDateTime) {
+		this.regDateTime = regDateTime;
+	}
+	public Date getRegDateTimeSvr() {
+		return regDateTimeSvr;
+	}
+	public void setRegDateTimeSvr(Date regDateTimeSvr) {
+		this.regDateTimeSvr = regDateTimeSvr;
+	}
+	public String getModIp() {
+		return modIp;
+	}
+	public void setModIp(String modIp) {
+		this.modIp = modIp;
+	}
+	public Integer getModSeq() {
+		return modSeq;
+	}
+	public void setModSeq(Integer modSeq) {
+		this.modSeq = modSeq;
+	}
+	public Integer getModDeviceCd() {
+		return modDeviceCd;
+	}
+	public void setModDeviceCd(Integer modDeviceCd) {
+		this.modDeviceCd = modDeviceCd;
+	}
+	public Date getModDateTime() {
+		return modDateTime;
+	}
+	public void setModDateTime(Date modDateTime) {
+		this.modDateTime = modDateTime;
+	}
+	public Date getModDateTimeSvr() {
+		return modDateTimeSvr;
+	}
+	public void setModDateTimeSvr(Date modDateTimeSvr) {
+		this.modDateTimeSvr = modDateTimeSvr;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/hongcheol/march/modules/test/testVo.java
@@ -0,0 +1,60 @@
+package com.hongcheol.march.modules.test;
+
+import java.util.Date;
+
+public class testVo {
+
+	private Integer shOption;
+	private String shValue;
+	private Integer shDelNy;
+	private String modtDate;
+	private String startDate;
+	private String endDate;
+	private Integer shDate;
+	
+//	--------------------------------------------------
+	
+	public Integer getShOption() {
+		return shOption;
+	}
+	public void setShOption(Integer shOption) {
+		this.shOption = shOption;
+	}
+	public String getShValue() {
+		return shValue;
+	}
+	public void setShValue(String shValue) {
+		this.shValue = shValue;
+	}
+	public Integer getShDelNy() {
+		return shDelNy;
+	}
+	public void setShDelNy(Integer shDelNy) {
+		this.shDelNy = shDelNy;
+	}
+	public String getModtDate() {
+		return modtDate;
+	}
+	public void setModtDate(String modtDate) {
+		this.modtDate = modtDate;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public Integer getShDate() {
+		return shDate;
+	}
+	public void setShDate(Integer shDate) {
+		this.shDate = shDate;
+	}
+	
+}
